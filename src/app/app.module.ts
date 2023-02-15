a4af7d268729d6be2d04e73e4353fc2a2dae2386
ArnaudClaire <71237096+ArnaudClaire@users.noreply.github.com>
2023-02-15 11:24:32 +0100

    add some elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/Meduza-home',
+      },
+      {
+        path: 'Meduza-home',
+        component: HomeComponent,
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
